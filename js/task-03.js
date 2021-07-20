744cf2e22ba0d2ab2e4378defc73b770d1038087
Oleksandr Okhrimovich <ohrimovich.sanek@gmail.com>
2021-07-20 17:04:55 +0300

    Update task-03.js

--- js/task-03.js
+++ js/task-03.js
@@ -15,8 +15,8 @@ const images = [
 const ulRef = document.querySelector('#gallery');
 const createGallery = arr => {
   const gallery = arr.map(item => {
-    `<li class='gallery-item'><img src='${item.url}' alt='${item.alt}' width='450'></li>`.join('');
-  });
+    `<li class='gallery-item'><img src='${item.url}' alt='${item.alt}' width='450'></li>`
+  })..join('');
   return gallery;
 }
  ulRef.insertAdjacentHTML('beforeend', createGallery(images));
